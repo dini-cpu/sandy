c1660a57775ca84eab33fcbb28972658b81ae9e5
dini-cpu <play42@naver.com>
2025-02-01 06:41:04 +0900

    모바일 최적화 테스트

--- colors.js
+++ colors.js
@@ -1,23 +1,23 @@
 function nightDayHandler(self){
-    var target =  document.querySelector('body') ;
+  var target =  document.querySelector('body') ;
 if(self.value === 'night'){
-  target.style.backgroundColor = 'black';
-  target.style.color = 'white';
-  self.value = 'day';
+target.style.backgroundColor = 'SlateBlue;';
+target.style.color = 'black';
+self.value = 'day';
 
-var alist = document.querySelectorAll('a');
+var alist = document.querySelectorAll('body');
 var i = 0;
 while (i < alist.length) {
-  alist[i].style.color = 'white'
+alist[i].style.color = 'white'
 i = i + 1;
 }
 }else{
-  target.style.backgroundColor = 'white';
-  target.style.color = 'black';
-  self.value = 'night';
-  var alist = document.querySelectorAll('a');
+target.style.backgroundColor = 'SlateBlue;';
+target.style.color = 'white';
+self.value = 'night';
+var alist = document.querySelectorAll('body');
 var i = 0;
 while (i < alist.length) {
-  alist[i].style.color = 'blue'
+alist[i].style.color = 'black'
 i = i + 1;
 }}}
