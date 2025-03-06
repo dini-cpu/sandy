9d0953e28f280beca62571b3d7bd2d67117ec5ad
dini-cpu <play42@naver.com>
2025-03-06 12:17:11 +0900

    타이핑 추가

--- colors.js
+++ colors.js
@@ -1,27 +1,23 @@
 function nightDayHandler(self){
-  var target =  document.querySelector('body') ;
-if(self.value === 'night'){
-target.style.backgroundColor = 'SlateBlue;';
-target.style.color = 'black';
-self.value = 'day';
-
-var alist = document.querySelectorAll('body');
-var i = 0;
-while (i < alist.length) {
-alist[i].style.color = 'white'
-i = i + 1;
-}
-}else{
-target.style.backgroundColor = 'SlateBlue;';
-target.style.color = 'white';
-self.value = 'night';
-var alist = document.querySelectorAll('body');
-var i = 0;
-while (i < alist.length) {
-alist[i].style.color = 'black'
-i = i + 1;
-}}}
-
-
-
-
+    var target =  document.querySelector('body') ;
+  if(self.value === 'night'){
+  target.style.backgroundColor = 'SlateBlue;';
+  target.style.color = 'black';
+  self.value = 'day';
+  
+  var alist = document.querySelectorAll('body');
+  var i = 0;
+  while (i < alist.length) {
+  alist[i].style.color = 'white'
+  i = i + 1;
+  }
+  }else{
+  target.style.backgroundColor = 'SlateBlue;';
+  target.style.color = 'white';
+  self.value = 'night';
+  var alist = document.querySelectorAll('body');
+  var i = 0;
+  while (i < alist.length) {
+  alist[i].style.color = 'black'
+  i = i + 1;
+  }}}
---
+++ typingEffect.js
@@ -0,0 +1,31 @@
+// 타이핑 효과를 주기 위한 함수
+function typeText(element, text, delay) {
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            // 줄 바꿈 처리
+            if (text.charAt(index) === '\n') {
+                element.innerHTML += '<br>'; // <br> 태그로 줄 바꿈
+                index++;
+            } else {
+                element.innerHTML += text.charAt(index);
+                index++;
+            }
+            setTimeout(type, delay);
+        }
+    }
+    type();
+}
+
+// 페이지가 로드될 때 모든 <p> 태그에 대해 타이핑 효과 적용
+window.onload = function() {
+    const paragraphs = document.querySelectorAll('p');
+    paragraphs.forEach((paragraph, index) => {
+        const text = paragraph.innerHTML.replace(/<br\s*\/?>/g, '\n'); // <br> 태그를 \n으로 변환
+        paragraph.innerHTML = ''; // 초기화
+        setTimeout(() => {
+            typeText(paragraph, text, 50); // 타이핑 효과 적용
+        }, index * 3000); // 각 문단의 시작 시간을 조정
+    });
+};
